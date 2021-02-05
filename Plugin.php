7e2a8399d4ffdd8eeecc14dba5cdaa41fc39438c
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-02-05 16:31:49 -0300

    commit inicial

---
+++ Plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PrestacaoDeContas;
+
+class Plugin extends \MapasCulturais\Plugin
+{
+    function __construct(array $config = [])
+    {
+        parent::__construct($config);
+    }
+
+    function _init()
+    {
+    }
+
+    function register()
+    {
+    }
+}
